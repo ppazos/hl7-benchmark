3f399b19ca068545fa03353022f180fba79cb7ec
Pablo Pazos <pablo.pazos@cabolabs.com>
2015-05-15 01:37:16 -0300

    added build scripts

---
+++ build.bat
@@ -0,0 +1,2 @@
+del bin\*.* /Q
+groovyc -cp .;%GROOVY_HOME%\embeddable\groovy-all-2.4.3.jar;lib\commons-cli-1.2.jar;lib\commons-collections-3.2.1.jar;lib\commons-logging-1.2.jar;lib\http-builder-0.5.2.jar;lib\httpclient-4.4.1.jar;lib\httpcore-4.4.1.jar;lib\json-lib-2.3-jdk15.jar;lib\xml-resolver-1.2.jar -d bin -j -Jsourcepath src src/com/cabolabs/hl7benchmark/Main.groovy
---
+++ jargen.bat
@@ -0,0 +1,1 @@
+jar -cfv hl7-benchmark.jar -C bin .
